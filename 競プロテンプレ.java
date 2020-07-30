9a2a3aead07d70cd319e8466f8c41cd465e0d792
便利屋わたり <51921469+WatariMayCry2@users.noreply.github.com>
2020-07-31 01:42:19 +0900

    importにスペース区切り追加

--- 競プロテンプレ.java
+++ 競プロテンプレ.java
@@ -1,4 +1,4 @@
-import java.util.*;import java.io.*;import java.math.*;
+import java.util.*; import java.io.*; import java.math.*;
 public class Main{
 	//Don't have to see. start------------------------------------------
 	static class InputIterator{
