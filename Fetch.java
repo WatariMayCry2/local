bd3b21ddec770bc723b4d0b658a6f1118450bc95
便利屋わたり <51921469+WatariMayCry2@users.noreply.github.com>
2020-09-30 00:06:07 +0900

    出力も取得する。また「multiTest」ディレクトリはやめて入力用、出力用のフォルダに振り分ける

--- Fetch.java
+++ Fetch.java
@@ -2,18 +2,22 @@ import java.net.*;
 import java.util.*;
 import java.io.*;
 public class Fetch{
-	static final String MULTI_TEST_DIR_STRING = "./multiTest";
+	static final String INPUT_DIR_STRING = "Sample Input";
+	static final String OUTPUT_DIR_STRING = "Sample Output";
 	Fetch(){}
 	public static void main(String[] args){
 		try{
-			File multiTestDir = new File(MULTI_TEST_DIR_STRING);
-			if(!multiTestDir.exists()){
-				multiTestDir.mkdir();
-			}
-			String[] multiTestFiles = multiTestDir.list();
-			for(int i = 0; i < multiTestFiles.length; i++){
-				File tmpFile = new File(MULTI_TEST_DIR_STRING + multiTestFiles[i]);
-				tmpFile.delete();
+			String[] dirs = {INPUT_DIR_STRING, OUTPUT_DIR_STRING};
+			for(int i = 0; i < dirs.length; i++){
+				File tmpDir = new File("./" + dirs[i]);
+				if(!tmpDir.exists()){
+					tmpDir.mkdir();
+				}
+				String[] multiTestFiles = tmpDir.list();
+				for(int j = 0; j < multiTestFiles.length; j++){
+					File tmpFile = new File("./" + dirs[i] + multiTestFiles[j]);
+					tmpFile.delete();
+				}
 			}
 			Scanner sc = new Scanner(System.in);
 			URL url = new URL(sc.next());
@@ -23,27 +27,35 @@ public class Fetch{
 			String read;
 			int count = 0;
 			boolean readnow = false;
+			int inputType = 0;//1:in 2:out
 			StringBuilder sb = new StringBuilder("");
 			while((read = bf.readLine()) != null){
-				if(!readnow && read.contains("Sample Input")){
+				if(!readnow && (read.contains(INPUT_DIR_STRING) || read.contains(OUTPUT_DIR_STRING))){
+					if(read.contains(INPUT_DIR_STRING)){
+						inputType = 1;
+						count++;
+					}else{
+						inputType = 2;
+					}
 					readnow = true;
-					count++;
-					read = read.replace("<h3>Sample Input " + count + "</h3><pre>", "");
-					System.out.println(read);
+					String replaceMae = "<h3>" + ((inputType == 1) ? INPUT_DIR_STRING : OUTPUT_DIR_STRING) + " " + count + "</h3><pre>";
+					read = read.replace(replaceMae, "");
 					sb.append(read + "\n");
 					continue;
 				}
 				if(readnow){
-					if(read.equals("</pre>")){
+					if(read.contains("</pre>")){
 						readnow = false;
-						File testFile = new File("./multiTest/" + count + ".txt");
+						File testFile = new File("./" + ((inputType == 1) ? INPUT_DIR_STRING : OUTPUT_DIR_STRING) + "/" + count + ".txt");
 						testFile.createNewFile();
 						FileWriter fileWriter = new FileWriter(testFile);
 						fileWriter.write(sb.toString());
 						fileWriter.close();
+						System.out.println("TestCase" + count);
+						System.out.println(sb.toString());
 						sb = new StringBuilder("");
+						inputType = 0;
 					}else{
-						System.out.println(read);
 						sb.append(read + "\n");
 					}
 				}
