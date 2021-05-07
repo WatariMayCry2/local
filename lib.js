0df8f3c0ef497e646e6df56b5df488e80701a8bd
便利屋わたり <51921469+WatariMayCry2@users.noreply.github.com>
2021-05-07 18:02:51 +0900

    BigIntに対応した平方根を追加

--- lib.js
+++ lib.js
@@ -322,3 +322,24 @@ function ArrayDeque(add){
 	}
 	return queue;
 }
+
+//BigIntに対応した平方根
+function mysqrt(value) {
+    if (value < 0n) {
+        throw 'square root of negative numbers is not supported'
+    }
+
+    if (value < 2n) {
+        return value;
+    }
+
+    function newtonIteration(n, x0) {
+        const x1 = ((n / x0) + x0) >> 1n;
+        if (x0 === x1 || x0 === (x1 - 1n)) {
+            return x0;
+        }
+        return newtonIteration(n, x1);
+    }
+
+    return newtonIteration(value, 1n);
+}
